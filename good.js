37b81faf012516947fd2df462d8772f5d7006aee
Shirashoji <tkm.shira@gmail.com>
2020-06-28 09:52:09 +0900

    commit

--- good.js
+++ good.js
@@ -1,7 +1,10 @@
 setInterval(
     function () {
+      var runlist = ["/home", "/notifications"];
+      if (runlist.indexOf(location.pathname) >= 0){
         var likes = document.body.querySelectorAll('div[data-testid="like"]');
         for (var i = 0; i < likes.length; i++) {
             likes[i].click();
         };
+      };
     }, 1000);
